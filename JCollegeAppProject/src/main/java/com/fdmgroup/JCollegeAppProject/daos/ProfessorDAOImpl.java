c440734c4b68f6dc84ab3d06234ac34ccc5f114f
Antoine.Fontaine <Antoine.Fontaine@fdmgroup.com>
2017-10-24 13:04:51 +0100

    Professor
    professorDAO
    professorDAOImpl
    completed

--- JCollegeAppProject/src/main/java/com/fdmgroup/JCollegeAppProject/daos/ProfessorDAOImpl.java
+++ JCollegeAppProject/src/main/java/com/fdmgroup/JCollegeAppProject/daos/ProfessorDAOImpl.java
@@ -1,5 +1,90 @@
 package com.fdmgroup.JCollegeAppProject.daos;
 
-public class ProfessorDAOImpl {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fdmgroup.JCollegeAppProject.entities.Course;
+import com.fdmgroup.JCollegeAppProject.entities.Department;
+import com.fdmgroup.JCollegeAppProject.entities.Professor;
+
+
+public class ProfessorDAOImpl implements ProfessorDAO {
+
+	@Autowired
+	private EntityManagerFactory factory;
+
+	public ProfessorDAOImpl() {
+	}
+
+	public ProfessorDAOImpl(EntityManagerFactory factory) {
+		this.factory = factory;
+	}
+
+	@Override
+	public void addProfessor(Professor professor) {
+		EntityManager manager = factory.createEntityManager();
+		manager.getTransaction().begin();
+		manager.persist(professor);
+		manager.getTransaction().commit();
+	}
+
+	@Override
+	public void removeProfessor(int professorId) {
+		EntityManager manager = factory.createEntityManager();
+		Professor professor = manager.find(Professor.class, professorId);
+		manager.getTransaction().begin();
+		manager.remove(professor);
+		manager.getTransaction().commit();
+	}
+
+	@Override
+	public void updateProfessor(Professor professor) {
+		EntityManager manager = factory.createEntityManager();
+		manager.getTransaction().begin();
+		manager.merge(professor);
+		manager.getTransaction().commit();
+	}
+
+	@Override
+	public Professor getProfessor(int professorId) {
+		EntityManager manager = factory.createEntityManager();
+		Professor professor = manager.find(Professor.class, professorId);
+		manager.close();
+		return professor;
+	}
+
+	@Override
+	public List<Professor> getAllProfessors() {
+		EntityManager manager = factory.createEntityManager();
+		TypedQuery<Professor> query = manager.createQuery("FROM Professor p", Professor.class);
+		List<Professor> professorList = query.getResultList();
+		manager.close();
+		return professorList;
+	}
+
+	@Override
+	public Professor getProfessorByCourse(Course course) {
+		EntityManager manager = factory.createEntityManager();
+		TypedQuery<Professor> query = manager.createQuery("FROM Professor p WHERE p.course =?", Professor.class);
+		query.setParameter(1, course);
+		Professor professor = query.getSingleResult();
+		manager.close();
+		return professor;
+	}
+
+	@Override
+	public List<Professor> getAllProfessorsByDepartment(Department department) {
+		EntityManager manager = factory.createEntityManager();
+		TypedQuery<Professor> query = manager.createQuery("FROM professor p WHERE p.department =?", Professor.class);
+		query.setParameter(1, department);
+		List<Professor> professorList = query.getResultList();
+		manager.close();
+		return professorList;
+	}
 
 }
--- JCollegeAppProject/src/main/java/com/fdmgroup/JCollegeAppProject/daos/StudentDAO.java
+++ JCollegeAppProject/src/main/java/com/fdmgroup/JCollegeAppProject/daos/StudentDAO.java
@@ -7,12 +7,18 @@ import com.fdmgroup.JCollegeAppProject.entities.Professor;
 import com.fdmgroup.JCollegeAppProject.entities.Student;
 
 public interface StudentDAO {
-	
+
 	public void addStudent(Student student);
+
 	public void removeStudent(int studentId);
+
 	public void updateStudent(Student student);
+
 	public Student getStudent(int studentId);
+
 	public List<Student> getAllStudents();
+
 	public List<Student> getAllStudentsByProfessor(Professor professor);
+
 	public List<Student> getAllStudentsByCourse(Course course);
 }
