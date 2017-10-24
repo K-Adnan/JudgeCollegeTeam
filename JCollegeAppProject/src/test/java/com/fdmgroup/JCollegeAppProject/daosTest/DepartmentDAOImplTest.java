4dd887b669a33a884e7a1e0fd24d34fa6f0ef8ae
Darryl.Chizh <Darryl.Chizh@fdmgroup.com>
2017-10-24 17:11:28 +0100

    Initialising DAO Testing

--- JCollegeAppProject/src/test/java/com/fdmgroup/JCollegeAppProject/daosTest/DepartmentDAOImplTest.java
+++ JCollegeAppProject/src/test/java/com/fdmgroup/JCollegeAppProject/daosTest/DepartmentDAOImplTest.java
@@ -1,5 +1,10 @@
 package com.fdmgroup.JCollegeAppProject.daosTest;
 
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -8,8 +13,10 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import com.fdmgroup.JCollegeAppProject.daos.DepartmentDAOImpl;
+import com.fdmgroup.JCollegeAppProject.entities.Department;
 
 public class DepartmentDAOImplTest {
 
@@ -26,6 +33,27 @@ public class DepartmentDAOImplTest {
 		manager = mock(EntityManager.class);
 		transaction = mock(EntityTransaction.class);
 		query = mock(TypedQuery.class);
+		departmentDao = new DepartmentDAOImpl(factory);
+		when(factory.createEntityManager()).thenReturn(manager);
+		when(manager.getTransaction()).thenReturn(transaction);
 	}
+	@Test
+	public void testAddingDepartmentsOntoDatabase(){
+		Department department = new Department();
+		//departmentDao.addDepartment(department);
+		verify(departmentDao).addDepartment(department);
+	}
+	@Test
+	public void testUpdatingDepartmentOnDatabase(){
+		Department department = new Department();
+		verify(departmentDao).updateDepartment (department);
+	}
+	@Test
+	public void testRemoveDepartmentFromDatabase(){
+		Department department = new Department ();
+		departmentDao.removeDepartment(departmentId);
+		
+	}
+	}
+	
 
-}
